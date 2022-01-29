8206f6d802839666fa1befd4f152c6d5fd445eb3
Tanja <G00411329@gmit.ie>
2022-01-29 21:05:05 +0000

    lecture codes of week02

--- week02/addOne.py
+++ week02/addOne.py
@@ -5,4 +5,4 @@
 number = int(input('Please enter a number: '))
 newNumber = number + 1
 
-print ('{} plus one is {}.'.format(number, newNumber))
+print ('{} plus one is {}.  '.format(number, newNumber))
